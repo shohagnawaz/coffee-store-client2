265f833546c663322abfa6ba43262209645a95ae
shohag nawaz <snawazdigital@gmail.com>
2024-06-06 23:16:01 +0600

    handle

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,8 +1,21 @@
 const AddCoffee = () => {
+  const handleAddCoffee = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+    const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+    console.log(newCoffee)
+  }
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h2 className="text-3xl font-extrabold">AddCoffee</h2>
-      <form>
+      <form onSubmit={handleAddCoffee}>
         {/* form name & quantity row */}
         <div className="md:flex">
           <div className="form-control md:w-1/2">
